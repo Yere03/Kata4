75dbf9ba1a6a10e32163af32ae95d34858352c57
Yeremay <1yeremay1@gmail.com>
2023-12-20 19:57:02 +0000

    RegisterProcessor created

---
+++ src/main/java/software/ulpgc/Kata4/RegisterProcessor.java
@@ -0,0 +1,8 @@
+package software.ulpgc.Kata4;
+
+import java.util.List;
+import java.util.Map;
+
+public interface RegisterProcessor {
+    Map<Integer,Integer> process(List<Register> registers);
+}
